e31616301aaf8502ccd3dee3cdc63fcc51d36131
carloeusebi <carloeusebi@gmail.com>
2023-05-30 15:20:09 +0200

    Added script comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-console.log('JSOK');
-
 const matrix = document.getElementById('FizzBuzzMatrix');
 
 let listOfElements = '';
@@ -18,21 +16,23 @@ for (let i = 1; i < 101; i++) {
     if (!(i % 3)) {
         elementToPrint += 'Fizz'
         classes += ' fizz';
-    };
+    }
 
     // if i is divisible by 5 add Buzz to elementToPrint
     if (!(i % 5)) {
         elementToPrint += 'Buzz'
         classes += ' buzz';
-    };
+    }
 
     // if i is neither divisble by 3 nor 5 elementToPrint is empty => we can print i
     if (!elementToPrint) elementToPrint = i;
 
-    console.log(elementToPrint);
+    console.log(elementToPrint); //print on console
 
+    // build html for the element
     listElement = `<div class="${classes}">${elementToPrint}</div>`;
 
+    // add new html at the end of list
     listOfElements += listElement;
 }
 
